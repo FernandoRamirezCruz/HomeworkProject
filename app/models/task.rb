93aa2ddd1b3db6c61a79bad48ac20d3bc813be24
FernandoRamirezCruz <135540022+FernandoRamirezCruz@users.noreply.github.com>
2025-01-24 18:01:02 -0600

    Nuevas validaciones en Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,16 @@ class Task < ApplicationRecord
   belongs_to :subject
   has_one_attached :img
   
+
+  validates :title, presence: true
+  validates :subject_id, presence: true
+
+  validate :date_cannot_be_in_the_past 
+
+  def date_cannot_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
+  
 end
